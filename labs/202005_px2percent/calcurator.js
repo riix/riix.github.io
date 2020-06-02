e802fe57e427de760657d479ee5225bfbfda1989
riix <putyourhandsup@gmail.com>
2020-06-02 10:25:16 +0900

    calcurator

--- labs/202005_px2percent/calcurator.js
+++ labs/202005_px2percent/calcurator.js
@@ -1,8 +1,14 @@
 $(function(){
-    var $app = $('#app');
-    var $input = $('input[type="number"], input[type="text"]');
-    var $textarea = $('textarea').eq(0);
+
+    var $document = $(document),
+        $app = $('#app'),
+        $input = $('fieldset input[type="number"], fieldset input[type="text"]'),
+        $textarea = $('textarea').eq(0),
+        $imageSrc = $('#imageSrc'),
+        $image = $('#image');
+
     var _props = {
+        imageSrc: '',
         width: 0,
         height: 0,
         top: 0,
@@ -12,75 +18,129 @@ $(function(){
         dataAttr: '',
         dataAttrVal: ''
     };
+
     var _values = {};
 
-    function getValue(_el){
+    function getInputValue(_el){ // input 객체 값 구하기
         var _result = $(_el).val();
         return _result;
     };
-    function setValue(_val){
-        _val = Math.round(_val.toFixed(3) * 100) / 100; // 소숫점 2자리 이하 절삭
-        return _val;
+    function returnRound(_val){ // 소숫점 2자리 이하 절삭
+        var _result = Math.round(_val.toFixed(3) * 100) / 100;
+        return _result;
     };
-    var setStorage = function(_arr){
+    function setStorage(_arr){
+        console.info(_arr);
         localStorage.setItem('calc', JSON.stringify(_arr));
     };
-    var getStorage = function(){
-        var _arr = JSON.parse(localStorage.getItem('calc'));
-        if (_arr == null) return false;
-        for (var property in _arr) {
-            if (_arr.hasOwnProperty(property)) {
-                $('#' + property).val(_arr[property]);
+    function getStorage(){
+        var obj = JSON.parse(localStorage.getItem('calc'));
+        if (obj == null) return false;
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                $('#' + prop).val(obj[prop]);
             }
         }
     };
 
+    var onInputImage = function(e){
+        _props.imageSrc = getInputValue('#imageSrc');
+        $image.attr('src', _props.imageSrc);
+        setStorage(_props);
+        $image.on('load', function(){
+            var _width = $image[0].naturalWidth;
+            var _height = $image[0].naturalHeight;
+            _props.width = _width;
+            _props.height = _height;
+            $('#width').val(_width);
+            $('#height').val(_height);
+            setStorage(_props);
+        });
+    };
+    $imageSrc.on('keypress focusout', function(e){
+        onInputImage();
+    });
+
+    var getProperties = function(){
+        var _result = {
+            imageSrc: getInputValue('#imageSrc'),
+            width: getInputValue('#width'),
+            height: getInputValue('#height'),
+            top: getInputValue('#top'),
+            left: getInputValue('#left'),
+            right: getInputValue('#right'),
+            bottom: getInputValue('#bottom'),
+            dataAttr: getInputValue('#dataAttr'),
+            dataAttrVal: getInputValue('#dataAttrVal')
+        };
+        return _result;
+    };
+
+    var getCalcuratedValue = function(_arrProps){
+        var _result = {
+            width: 0,
+            height: 0,
+            left: _arrProps.left / _arrProps.width * 100,
+            top: _arrProps.top / _arrProps.height * 100,
+            right: _arrProps.right / _arrProps.width * 100,
+            bottom: _arrProps.bottom / _arrProps.height * 100
+        };
+        _result.width = _result.right - _result.left;
+        _result.height = _result.bottom - _result.top;
+        return _result;
+    };
+
+    var getInlineStyle = function(_styles){
+        var _result = 'left: ' + _styles.left + '; top: ' + _styles.top + '; width: ' + _styles.width + '; height: ' + _styles.height + ';';
+        return _result;
+    };
+
     var result = function(e){
 
-        _props.width = getValue('#width');
-        _props.height = getValue('#height');
-        _props.top = getValue('#top');
-        _props.left = getValue('#left');
-        _props.right = getValue('#right');
-        _props.bottom = getValue('#bottom');
-        _props.dataAttr = $('#dataAttr').val();
-        _props.dataAttrVal = $('#dataAttrVal').val();
+        _props = getProperties();
+        _values = getCalcuratedValue(_props);
 
-        var _result = '<a class="gaip" href="javascript:;" style="';
+        var _styles = {
+            left: returnRound(_values.left) + '%',
+            top: returnRound(_values.top) + '%',
+            width: returnRound(_values.width) + '%',
+            height: returnRound(_values.height) + '%'
+        };
+
         var _position = _props.left + '|' + _props.top + '|' + _props.right + '|' + _props.bottom;
 
-        _values.left = _props.left / _props.width * 100;
-        _values.top = _props.top / _props.height * 100;
-        _values.right = _props.right / _props.width * 100;
-        _values.bottom = _props.bottom / _props.height * 100;
-        _values.width = _values.right - _values.left;
-        _values.height = _values.bottom - _values.top;
+        var _html = [
+            '<a class="gaip" href="javascript:;" style="' + getInlineStyle(_styles) + '"',
+            ' data-' + _props.dataAttr + '="'+ _props.dataAttrVal + '" data-position="' + _position + '">',
+            _props.dataAttrVal + '</a>'
+        ];
+        _html = _html.join('');
 
-        _result += 'left:' + setValue(_values.left) + '%; ';
-        _result += 'top:' + setValue(_values.top) + '%; ';
-        _result += 'width:' + setValue(_values.width) + '%; ';
-        _result += 'height:' + setValue(_values.height) + '%;';
-        _result += '" ';
-        _result += 'data-' + _props.dataAttr + '="' + _props.dataAttrVal + '" ';
-        _result += 'data-position="' + _position + '"';
-        _result += '>' + _props.dataAttrVal +'</a>';
+        $app.find('a').remove(); // 버튼 재배치
+        $app.append($(_html));
+
+        $textarea.val(_html); // 결과값 노출
 
         setStorage(_props);
 
-        $textarea.val(_result);
-        $app.find('a').remove();
-        $app.append($(_result));
     };
-    $input.on('focusout', function(e){
-        result(e);
-    });
-    $('textarea').on('focus', function(e){
-        $(e.target).select();
-    });
-    $('#file').on('change', function(e){
-        var _file = $(e.target)[0].files[0].name;
-        $('#image').attr('src', _file);
-    });
-    getStorage();
-    result();
+
+    var setHandler = function(){
+        $input.on('keyup', function(e){
+            result();
+        });
+        $('textarea').on('focus', function(e){
+            $(e.target).select();
+        });
+    };
+
+    var init = function(){
+        getStorage();
+        result();
+        setHandler();
+        $image.attr('src', _props.imageSrc);
+    };
+
+    init();
+
 });
